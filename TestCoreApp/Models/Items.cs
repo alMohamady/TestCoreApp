9834461fefc32f8175970cc07d85e63a5294a317
al Mohamady <al.Mohamady47@gmail.com>
2022-10-11 15:00:10 +0200

    Add project files.

---
+++ TestCoreApp/Models/Items.cs
@@ -0,0 +1,10 @@
+﻿namespace TestCoreApp.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
