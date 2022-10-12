e94ee1bd8cf66d77fb728913a62276d4e3ca1d8a
al Mohamady <al.Mohamady47@gmail.com>
2022-10-12 16:22:33 +0200

    019 Add Items List Screen

--- TestCoreApp/Controllers/ItemsController.cs
+++ TestCoreApp/Controllers/ItemsController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TestCoreApp.Data;
+using TestCoreApp.Models;
 
 namespace TestCoreApp.Controllers
 {
     public class ItemsController : Controller
     {
+        public ItemsController(AppDbContext db)
+        {
+            _db = db;
+        }   
+        
+        private readonly AppDbContext _db;
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Item> itemsList = _db.Items.ToList();
+            return View(itemsList);
         }
     }
 }
