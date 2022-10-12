8872b34edc66098a19b392a4f0cff98c6920df24
al Mohamady <al.Mohamady47@gmail.com>
2022-10-12 14:39:44 +0200

    018 Create Controller

---
+++ TestCoreApp/Controllers/ItemsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestCoreApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
