1ca51f2909f949a99e08aa2312ae2cef15eceba6
al Mohamady <al.Mohamady47@gmail.com>
2022-11-12 14:05:02 +0200

    030 Useing Select option tool

--- TestCoreApp/Controllers/ItemsController.cs
+++ TestCoreApp/Controllers/ItemsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using TestCoreApp.Data;
 using TestCoreApp.Models;
 
--- TestCoreApp/Models/Item.cs
+++ TestCoreApp/Models/Item.cs
@@ -15,5 +15,9 @@ namespace TestCoreApp.Models
         [Range(10, 1000, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public decimal Price { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [DisplayName("Category")]
+        public int CategoryId { get; set; }
     }
 }
